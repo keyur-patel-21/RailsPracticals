3d1082b00254d51a4318f32800f7a4bd5b128645
Keyur Patel <keyur.patel@simformsolutions.com>
2022-03-21 17:19:34 +0530

    feat: TE5-T90 implement callbacks in student and faculty models

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -6,6 +6,61 @@ class Faculty < ApplicationRecord
     message: "Faculty can not be %{value}" }, inclusion: { in: %w(Ass.Prof. Prof.), message: "Invalid faculty designation" }
   validate :birthdate_cannot_be_in_future
 
+  before_validation :check_validation
+  after_validation :complete_validation, :check_dob
+  after_save :complete_save, :check_email
+  before_update :updating
+  after_update :updated
+  after_destroy :deleted
+  after_validation :check_dob,if: Proc.new { |faculty| faculty.errors[:f_dob] == [] }
+
+  after_initialize do |faculty|
+    puts "You have initialized a faculty!"
+  end
+
+  after_find do |faculty|
+    puts "You have found a faculty!"
+  end
+
+  private
+
+    def check_validation
+      puts "checking for validation."
+    end
+
+    def complete_validation
+      puts "validation completed!!"
+    end
+
+    def complete_save
+      puts "faculty saved successfully!!"
+    end
+
+    def updating
+      puts "faculty is being edited"
+    end
+
+    def updated
+      puts "faculty is updated successfully!! "
+    end
+
+    def deliting
+      puts "faculty is being deleted!!"
+    end
+
+    def deleted
+      puts "faculty is deleted!!"
+    end
+
+    def check_dob
+      puts "faculty's Birthdate validated."
+    end
+
+    def check_email
+      puts "faculty's email validated."
+    end
+
+
   def birthdate_cannot_be_in_future
     if f_dob.present? && f_dob >= Date.today
       errors.add(:f_dob, "can't be in the past")
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,59 @@
 class Student < ApplicationRecord
+
+  after_initialize do |student|
+    puts "You have initialized a student!"
+  end
+
   validates :s_firstname, :s_lastname, :s_department, presence: true
   validates :s_department, inclusion: { in: %w(IT CE),message: "Department can't be the %{value}" }
   validates :s_terms, acceptance: true
   validate :birthdate_cannot_be_in_future
+
+  before_validation :check_validation 
+  before_validation :check_dob
+  after_validation :complete_validation
+  after_save :complete_save
+  before_update :updating
+  after_update :updated
+  after_destroy :deleted
+
+  after_find do |student|
+    puts "You have found a student!"
+  end
+
+  private
+
+    def check_validation
+      puts "checking for validation."
+    end
+
+    def check_dob
+      puts "studen's birthdate is being validated"
+    end
+
+    def complete_validation
+      puts "validation completed!!"
+    end
+
+    def complete_save
+      puts "student saved successfully!!"
+    end
+
+    def updating
+      puts "student is being edited"
+    end
+
+    def updated
+      puts "student is updated successfully!! "
+    end
+
+    def deliting
+      puts "student is being deleted!!"
+    end
+
+    def deleted
+      puts "student is deleted!!"
+    end
 
   def birthdate_cannot_be_in_future
     if s_dob.present? && s_dob > Date.today

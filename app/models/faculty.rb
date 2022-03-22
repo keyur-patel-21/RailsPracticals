60286a0b7c2ae5b8ef25d79bb369d630acf2ff88
Keyur Patel <keyur.patel@simformsolutions.com>
2022-03-22 14:29:40 +0530

    feat: TE5-T90 implement callbacks in student and faculty model

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,4 +1,5 @@
 class Faculty < ApplicationRecord
+
   validates :f_firstname, :f_lastname, :f_phone, :f_email, :f_dob, :f_designation, presence: true
   validates :f_phone, length: { minimum: 10 }, numericality: { only_integer: true } 
   validates :f_email, uniqueness: true
@@ -12,58 +13,56 @@ class Faculty < ApplicationRecord
   before_update :updating
   after_update :updated
   after_destroy :deleted
-  after_validation :check_dob,if: Proc.new { |faculty| faculty.errors[:f_dob] == [] }
-
+  
   after_initialize do |faculty|
-    puts "You have initialized a faculty!"
+    puts "----You have initialized a faculty!----"
   end
 
   after_find do |faculty|
-    puts "You have found a faculty!"
+    puts "----You have found a faculty!----"
+  end
+
+  after_touch do
+    puts '----An Employee was touched----'
   end
 
   private
 
     def check_validation
-      puts "checking for validation."
+      puts "----checking for validation.----"
     end
 
     def complete_validation
-      puts "validation completed!!"
+      puts "----validation completed!!----"
     end
 
     def complete_save
-      puts "faculty saved successfully!!"
-    end
-
-    def updating
-      puts "faculty is being edited"
-    end
-
-    def updated
-      puts "faculty is updated successfully!! "
-    end
-
-    def deliting
-      puts "faculty is being deleted!!"
-    end
-
-    def deleted
-      puts "faculty is deleted!!"
+      puts "----faculty saved successfully!!----"
     end
 
     def check_dob
-      puts "faculty's Birthdate validated."
+      puts "----faculty's Birthdate validated.----"
     end
 
     def check_email
-      puts "faculty's email validated."
+      puts "----faculty's email validated.----"
     end
 
+    def updating
+      puts "----faculty is being edited----"
+    end
+
+    def updated
+      puts "----faculty is updated successfully!! ----"
+    end
+
+    def deleted
+      puts "----faculty has been deleted!!----"
+    end
 
   def birthdate_cannot_be_in_future
     if f_dob.present? && f_dob >= Date.today
-      errors.add(:f_dob, "can't be in the past")
+      errors.add(:f_dob, "can't be in the future")
     end
   end  
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,5 @@
 class Student < ApplicationRecord
 
-  after_initialize do |student|
-    puts "You have initialized a student!"
-  end
-
   validates :s_firstname, :s_lastname, :s_department, presence: true
   validates :s_department, inclusion: { in: %w(IT CE),message: "Department can't be the %{value}" }
   validates :s_terms, acceptance: true
@@ -15,49 +11,53 @@ class Student < ApplicationRecord
   after_save :complete_save
   before_update :updating
   after_update :updated
-  after_destroy :deleted
+  after_destroy :deleted, if: Proc.new { |student| student.s_department == "CE" }
+
+  after_initialize do |student|
+    puts "----You have initialized a student!----"
+  end
 
   after_find do |student|
-    puts "You have found a student!"
+    puts "----You have found a student!----"
+  end
+
+  after_touch do
+    puts '----An Employee was touched----'
   end
 
   private
 
     def check_validation
-      puts "checking for validation."
+      puts "----checking for validation.----"
     end
 
     def check_dob
-      puts "studen's birthdate is being validated"
+      puts "----studen's birthdate is being validated----"
     end
 
     def complete_validation
-      puts "validation completed!!"
+      puts "----validation completed!!----"
     end
 
     def complete_save
-      puts "student saved successfully!!"
+      puts "----student saved successfully!!----"
     end
 
     def updating
-      puts "student is being edited"
+      puts "----student is being edited----"
     end
 
     def updated
-      puts "student is updated successfully!! "
-    end
-
-    def deliting
-      puts "student is being deleted!!"
+      puts "----student is updated successfully!!---- "
     end
 
     def deleted
-      puts "student is deleted!!"
+      puts "----student has been deleted!!----"
     end
 
   def birthdate_cannot_be_in_future
     if s_dob.present? && s_dob > Date.today
-      errors.add(:s_dob, "can't be in the future")
+      errors.add(:s_dob, "----can't be in the future----")
     end
   end 
 end

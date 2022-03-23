76f48589c4c4960aac1a3aec72057c525749f52d
Keyur Patel <keyur.patel@simformsolutions.com>
2022-03-23 11:31:59 +0530

    fix: TE5-T90 add comment to code.

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,18 +1,20 @@
 class Faculty < ApplicationRecord
 
+  # validated faculties' fields.
   validates :f_firstname, :f_lastname, :f_phone, :f_email, :f_dob, :f_designation, presence: true
   validates :f_phone, length: { minimum: 10 }, numericality: { only_integer: true } 
-  validates :f_email, uniqueness: true
+  validates :f_email, confirmation:true , format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true
   validates :f_designation, exclusion: { in: %w(HOD Sr.Prof),
     message: "Faculty can not be %{value}" }, inclusion: { in: %w(Ass.Prof. Prof.), message: "Invalid faculty designation" }
   validate :birthdate_cannot_be_in_future
 
+  # implemented callbacks 
   before_validation :check_validation
   after_validation :complete_validation, :check_dob
   after_save :complete_save, :check_email
   before_update :updating
   after_update :updated
-  after_destroy :deleted
+  after_destroy :deleted 
   
   after_initialize do |faculty|
     puts "----You have initialized a faculty!----"
@@ -26,40 +28,50 @@ class Faculty < ApplicationRecord
     puts '----An Employee was touched----'
   end
 
+  # Methods for callbacks
   private
 
+    # calling before validation
     def check_validation
       puts "----checking for validation.----"
     end
 
+    # calling after validation
     def complete_validation
       puts "----validation completed!!----"
     end
 
+    # calling after object is saved.
     def complete_save
       puts "----faculty saved successfully!!----"
     end
 
+    # calling after faculty's birthdate validation.
     def check_dob
       puts "----faculty's Birthdate validated.----"
     end
 
+    # calling after faculty's email validated.
     def check_email
       puts "----faculty's email validated.----"
     end
 
+    # calling before object update.
     def updating
       puts "----faculty is being edited----"
     end
 
+    # calling after object is updated.
     def updated
       puts "----faculty is updated successfully!! ----"
     end
 
+    # calling after object is deleted.
     def deleted
       puts "----faculty has been deleted!!----"
     end
 
+  # custom method for validating birthdate.
   def birthdate_cannot_be_in_future
     if f_dob.present? && f_dob >= Date.today
       errors.add(:f_dob, "can't be in the future")
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,60 +1,78 @@
 class Student < ApplicationRecord
 
+  #validated students' field
   validates :s_firstname, :s_lastname, :s_department, presence: true
   validates :s_department, inclusion: { in: %w(IT CE),message: "Department can't be the %{value}" }
   validates :s_terms, acceptance: true
   validate :birthdate_cannot_be_in_future
 
+  # implemented callbacks 
   before_validation :check_validation 
   before_validation :check_dob
   after_validation :complete_validation
   after_save :complete_save
   before_update :updating
   after_update :updated
-  after_destroy :deleted, if: Proc.new { |student| student.s_department == "CE" }
+  after_destroy :deleted , if: :delete_count?
 
+  # Method that called after initializing
   after_initialize do |student|
     puts "----You have initialized a student!----"
   end
 
+  # Method that called after finding object
   after_find do |student|
     puts "----You have found a student!----"
   end
 
+  # Method taht called when object is touched.
   after_touch do
     puts '----An Employee was touched----'
   end
 
   private
 
+    # calling before validation
     def check_validation
       puts "----checking for validation.----"
     end
 
+    # calling before validation of Birth date.
     def check_dob
       puts "----studen's birthdate is being validated----"
     end
 
+    # Calling before after validation gets completed.
     def complete_validation
       puts "----validation completed!!----"
     end
 
+    # Calling after object saved.
     def complete_save
       puts "----student saved successfully!!----"
     end
 
+    # calling before object is updated.
     def updating
       puts "----student is being edited----"
     end
 
+    # calling after object is updated.
     def updated
       puts "----student is updated successfully!!---- "
     end
 
+    # calling after object is deleted
     def deleted
       puts "----student has been deleted!!----"
     end
+    
+    # conditional method for checking condition.
+    def delete_count?
+      Student.count >= 2 ? true : false
+    end
 
+    # custom method for validating birthdate
   def birthdate_cannot_be_in_future
     if s_dob.present? && s_dob > Date.today
       errors.add(:s_dob, "----can't be in the future----")

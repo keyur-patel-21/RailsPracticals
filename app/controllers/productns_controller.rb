75a547c10ccb670a1db5a8175c256f1ff15daacf
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-04-19 14:44:00 +0530

    style: TE%-T95 make change in format of code

--- app/controllers/productns_controller.rb
+++ app/controllers/productns_controller.rb
@@ -1,51 +1,51 @@
 class ProductnsController < ApplicationController
   
   before_action :set_productn, only: [:show, :edit, :update, :destroy]
-	
-	def index
+  
+  def index
     @productns = Productn.all
-	end
-	
-	def new
-		@productn = Productn.new
-	end
-	
-	def create
-		@productn = Productn.create(productn_params)
+  end
+  
+  def new
+    @productn = Productn.new
+  end
+  
+  def create
+    @productn = Productn.create(productn_params)
     if @productn.save
-			redirect_to productns_path
-		else
-			render "new"
-		end
-	end
-	
-	def edit
-	end
-	
-	def update
-		if @productn.update(productn_params)
-			redirect_to productn_path(@productn)
-		else
-			render "edit"
-		end
-	end
+      redirect_to productns_path
+    else
+      render "new"
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @productn.update(productn_params)
+      redirect_to productn_path(@productn)
+    else
+      render "edit"
+    end
+  end
 
-	def show
-	end
-	
-	def destroy
-		if @productn.destroy
-			redirect_to productns_path
-		end
-	end
+  def show
+  end
+  
+  def destroy
+    if @productn.destroy
+      redirect_to productns_path
+    end
+  end
 
-	private
+  private
 
-	def set_productn
-		@productn = Productn.find(params[:id])
-	end
+  def set_productn
+    @productn = Productn.find(params[:id])
+  end
 
-	def productn_params
-		params.require(:productn).permit(:name, :rate)
-	end
+  def productn_params
+    params.require(:productn).permit(:name, :rate)
+  end
 end

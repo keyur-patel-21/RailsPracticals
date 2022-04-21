351fcf054ab0572d902e3ca5ea7ba9cc8be733b2
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-04-21 14:33:59 +0530

    style : TE5-T96  chanegd code format

--- app/controllers/orderns_controller.rb
+++ app/controllers/orderns_controller.rb
@@ -1,56 +1,56 @@
 class OrdernsController < ApplicationController
 
   before_action :set_ordern, only: [:show, :edit, :update, :destroy]
-	
-	def index
+  
+  def index
     @orderns = Ordern.all
-	end
-	
-	def new
-		@productn = Productn.find(params[:productn_id])
-		@ordern = Ordern.new
-	end
-	
-	def create
-		@productn = Productn.find(params[:productn_id])
-		@ordern = Ordern.create(ordern_params)
+  end
+  
+  def new
+    @productn = Productn.find(params[:productn_id])
+    @ordern = Ordern.new
+  end
+  
+  def create
+    @productn = Productn.find(params[:productn_id])
+    @ordern = Ordern.create(ordern_params)
     if @ordern.save
-			redirect_to productn_path(@productn)
-		else
-			render "new"
-		end
-	end
-	
-	def edit
-		@productn = Productn.find(params[:productn_id])
-	end
-	
-	def update
-		@productn = Productn.find(params[:productn_id])
-		if @ordern.update(ordern_params)
-			redirect_to productn_path(@productn)
-		else
-			render "edit"
-		end
-	end
+      redirect_to productn_path(@productn)
+    else
+      render "new"
+    end
+  end
+  
+  def edit
+    @productn = Productn.find(params[:productn_id])
+  end
+  
+  def update
+    @productn = Productn.find(params[:productn_id])
+    if @ordern.update(ordern_params)
+      redirect_to productn_path(@productn)
+    else
+      render "edit"
+    end
+  end
 
-	def show
-	end
-	
-	def destroy
-		@productn = Productn.find(params[:productn_id])
-		if @ordern.destroy
-			redirect_to productn_path(@productn)
-		end
-	end
+  def show
+  end
+  
+  def destroy
+    @productn = Productn.find(params[:productn_id])
+    if @ordern.destroy
+      redirect_to productn_path(@productn)
+    end
+  end
 
-	private
+  private
 
-	def set_ordern
-		@ordern = Ordern.find(params[:id])
-	end
+  def set_ordern
+    @ordern = Ordern.find(params[:id])
+  end
 
-	def ordern_params
-		params.require(:ordern).permit(:productn_id, :quantity)
-	end
+  def ordern_params
+    params.require(:ordern).permit(:productn_id, :quantity)
+  end
 end

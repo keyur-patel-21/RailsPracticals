c3faeee155e5ecd49fd2224b251f04649fab96d7
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-05-04 16:32:34 +0530

    feat: TE5-T103 make association between user and products

--- app/models/myuser.rb
+++ app/models/myuser.rb
@@ -4,5 +4,7 @@ class Myuser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :test_products
+
   validates :role, presence: true
 end
--- app/models/test_product.rb
+++ app/models/test_product.rb
@@ -1,2 +1,5 @@
 class TestProduct < ApplicationRecord
+  belongs_to :myuser
+
+  validates :product_name, :price,  :description, presence: true
 end

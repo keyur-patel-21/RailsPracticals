c1a20766322f90df383e612b73c1a1ca8cf199ab
keyur-at-simform <keyur.patel@simformsolutions.com>
2022-05-11 10:38:26 +0530

    feat: TE5-T103 implement rspec test for product model

--- spec/models/test_product_spec.rb
+++ spec/models/test_product_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe TestProduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @user = Myuser.new(email: "keyur@gmail.com",role: "admin",password: "password")
+    @user.save
+    @product = TestProducts.new(product_name: "productxyz", price: "600", description: "adhdahah", myuser_id: @user.id)
+    @product.save
+  end
+  
+  it "is valid with valid attributes" do
+    expect(@product).to be_valid
+  end
+
+  it "is not valid without a product_name" do
+    @product.product_name = ""
+    expect(@product).to_not be_valid
+  end
+
+  it "is not valid without a price" do
+    @product.price = ""
+    expect(@product).to_not be_valid
+  end
+  
+  it "is not valid without a myuser id" do
+    @product.myuser_id = ""
+    expect(@product).to_not be_valid
+  end
 end
